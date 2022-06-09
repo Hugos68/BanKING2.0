d1d438c66acc335610e7519b90762705b7d555ba
hugos68 <hugokorteapple@gmail.com>
2022-06-09 21:44:53 +0200

    404 page done

--- src/web/js/404.js
+++ src/web/js/404.js
@@ -1,7 +1,5 @@
-
-
 document.addEventListener("DOMContentLoaded", async () => {
     setTimeout(function () {
         window.location.replace("../html/home.html");
-    }, 3000);
+    }, 4500);
 });
