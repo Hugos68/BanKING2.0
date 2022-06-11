226c41e45c0812578020faeec62285647e2e8770
hugos68 <hugokorteapple@gmail.com>
2022-06-11 16:16:24 +0200

    styling

--- src/web/js/home.js
+++ src/web/js/home.js
@@ -68,11 +68,3 @@ signInButton.addEventListener('click', async () => {
         throw new Error(e.message);
     }
 });
-
-
-const header = document.querySelector("header");
-
-// Mechanism to toggle navigation menu top of page is reached
-document.addEventListener("scroll", () => {
-    header.classList.toggle("header-pop-out", scrollY < 50);
-}, {passive: true});
