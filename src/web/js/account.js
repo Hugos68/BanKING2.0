07b8215b506d659c2f2b8c12d65634fb1c94ccf9
hugos68 <hugokorteapple@gmail.com>
2022-06-12 23:30:04 +0200

    More efficient cookie removal

--- src/web/js/account.js
+++ src/web/js/account.js
@@ -2,13 +2,9 @@ const signOutButton = document.querySelector(".sign-out-button");
 
 signOutButton.addEventListener('click', async () => {
 
-    // Create date 1 second from now
-    const date = new Date();
-    const expires = new Date(date.getSeconds()+1);
-
     // Set tokens to undefine to counter auto-login, set expires to now plus 1 second to expire them
-    document.cookie = "refresh_token=undefined; expires="+expires.toUTCString()+";";
-    document.cookie = "access_token=undefined; expires="+expires.toUTCString()+";";
+    document.cookie = "refresh_token=; Max-Age=-99999999;";
+    document.cookie = "access_token=; Max-Age=-99999999;";
 
     // Replace screen back to home
     location.replace("home.html");
