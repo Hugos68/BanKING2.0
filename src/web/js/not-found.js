8721d60a0a9f3ff52181281aabdd3a907fb54179
Hugos68 <hugokorteapple@gmail.com>
2022-06-12 13:02:20 +0200

    added better styling to 404 (fix created by pop)

--- src/web/js/not-found.js
+++ src/web/js/not-found.js
@@ -1,5 +1,7 @@
+
+// Redirect user back to home page after 2.5 seconds so that they can read the error message
 document.addEventListener("DOMContentLoaded", async () => {
     setTimeout(function () {
         window.location.replace("../html/home.html");
-    }, 4500);
+    }, 2500);
 });
