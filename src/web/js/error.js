e5b63f196383d88c0ec2cfd5e8685f5f8ec7a2d9
Hugos68 <hugokorteapple@gmail.com>
2022-06-23 19:54:32 +0200

    Cleanup

--- src/web/js/error.js
+++ src/web/js/error.js
@@ -1,8 +1,8 @@
 // Redirect user back to home page after 2.5 seconds so that they can read the error message
-await redirectToHome();
-
 async function redirectToHome() {
     setTimeout(function() {
         window.location.replace("../html/index.html");
     }, 2500);
 }
+
+await redirectToHome();
